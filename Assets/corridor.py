4013be2522d631b3b6559f4cfbed532ececa0a6e
anantharaman445 <anantharaman45@gmail.com>
2021-06-30 13:37:47 +0530

    corridors changed

--- Assets/corridor.py
+++ Assets/corridor.py
@@ -3,6 +3,8 @@ from Equipments.light import Light
 from Equipments.airconditioner import AirConditioner 
 
 class Corridor:
-    def __init__(self):
-        pass
+    def __init__(self, corridor_type):
+        self.light = Light()
+        self.airconditioner = AirConditioner()
+        self.corridor_type = corridor_type
     
--- Assets/subcorridor.py
+++ Assets/subcorridor.py
@@ -1,6 +1,8 @@
 from Utils import utils
 from Assets.corridor import Corridor
 
+corridor_type = utils.hotel_management_constants["CORRIDORTYPE"]
+
 class SubCorridor:
     def __init__(self):
-        pass
+        Corridor.__init__(self, corridor_type["SUBCORRIDOR"])
