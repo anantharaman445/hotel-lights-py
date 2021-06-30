cb35237b5e80f284de2c9053e12a2312055863bf
anantharaman445 <anantharaman45@gmail.com>
2021-06-30 14:30:23 +0530

    floor changes

---
+++ Assets/floor.py
@@ -0,0 +1,28 @@
+from Utils import utils
+from Assets.maincorridor import MainCorridor
+from Assets.subcorridor import SubCorridor
+
+corridor_type = utils.hotel_management_constants["CORRIDORTYPE"]
+
+class Floor:
+    def __init__(self, main_corridors_per_floor, sub_corridor_per_floor):
+        self.total_main_corridors = main_corridors_per_floor
+        self.total_sub_corridors = sub_corridor_per_floor
+        self.floor_map = self.get_corridors()
+
+    def get_corridors(self):
+        self.floor_map = dict()
+        self.get_main_corridors()
+        self.get_sub_corridors()
+        return self.floor_map
+
+
+    def get_main_corridors(self):
+        self.floor_map["main_corridors"] = []
+        for i in range(0, self.total_main_corridors):
+            self.floor_map["main_corridors"].append(MainCorridor())
+    
+    def get_sub_corridors(self):
+        self.floor_map["sub_corridors"] = []
+        for i in range(0, self.total_sub_corridors):
+            self.floor_map["sub_corridors"].append(SubCorridor())
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Utils import utils
 from Assets.maincorridor import MainCorridor 
 from Assets.subcorridor import SubCorridor
+from Assets.floor import Floor
 
 hotel_management_constants = utils.hotel_management_constants
 
@@ -9,6 +10,11 @@ hotel_management_constants = utils.hotel_management_constants
 # print(light.equipment_type, light.power_consumption, light.equipment_state)
 # print(air_conditioner.equipment_type, air_conditioner.power_consumption, air_conditioner.equipment_state)
 
-main_corridor = MainCorridor()
-print(main_corridor.airconditioner.equipment_type, main_corridor.airconditioner.power_consumption)
-print(main_corridor.corridor_type)
+# main_corridor = MainCorridor()
+# print(main_corridor.airconditioner.equipment_type, main_corridor.airconditioner.power_consumption)
+# print(main_corridor.corridor_type)
+
+floor = Floor(1, 2)
+main_corridor = floor.floor_map["main_corridors"][0]
+print(main_corridor.airconditioner.equipment_type, main_corridor.airconditioner.power_consumption, main_corridor.airconditioner.equipment_state)
+# print(floor.floor_map["main_corridors"][0])
