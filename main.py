9c60423923cab1b2243370b1afbfb9ba8504c8b7
anantharaman445 <anantharaman45@gmail.com>
2021-06-30 13:45:05 +0530

    main is tested

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from Utils import utils
-from Equipments.light import Light 
-from Equipments.airconditioner import AirConditioner 
+from Assets.maincorridor import MainCorridor 
+from Assets.subcorridor import SubCorridor
 
 hotel_management_constants = utils.hotel_management_constants
 
-light = Light()
-air_conditioner = AirConditioner()
-print(light.equipment_type, light.power_consumption, light.equipment_state)
-print(air_conditioner.equipment_type, air_conditioner.power_consumption, air_conditioner.equipment_state)
+# light = Light()
+# air_conditioner = AirConditioner()
+# print(light.equipment_type, light.power_consumption, light.equipment_state)
+# print(air_conditioner.equipment_type, air_conditioner.power_consumption, air_conditioner.equipment_state)
+
+main_corridor = MainCorridor()
+print(main_corridor.airconditioner.equipment_type, main_corridor.airconditioner.power_consumption)
+print(main_corridor.corridor_type)
