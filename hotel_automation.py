e603fd5646749602408203de31348a5aa8479f8b
anantharaman445 <anantharaman45@gmail.com>
2021-07-01 14:04:28 +0530

    automation script added

--- hotel_automation.py
+++ hotel_automation.py
@@ -1,7 +1,11 @@
-from Utils import utils, floor_utils
-from Operations.floor_operations import add_floors
+from Utils.floor_utils import add_floors
 
 
 class HotelAutomation:
     def __init__(self, no_of_floors, main_corridors_per_floor, sub_corridor_per_floor):
         self.floor_map = add_floors(no_of_floors, main_corridors_per_floor, sub_corridor_per_floor)
+
+    def time_slot_change(self):
+        for floor_id, floor in self.floor_map.items():
+            floor.floor_time_slot_shift()
+    
--- main.py
+++ main.py
@@ -1,35 +1,14 @@
 
-from Utils.floor_utils import add_floors
+# from Utils.floor_utils import add_floors
 
 
+from hotel_automation import HotelAutomation as ha
 
-floor_m = add_floors(1, 1, 2)
-# print(floor_m)
-floor = floor_m[1]
-# print(floor.maximum_allowed_units)
-print(floor.get_floor_equipment_units())
+yy = ha(1,1,2)
 
+print(yy.floor_map[1].get_floor_equipment_units())
 
-main_corridor = floor.floor_corridor_map["main_corridors"]
-for key, value in main_corridor.items():
-    print(value.light.equipment_type, value.light.equipment_state)
-    print(value.airconditioner.equipment_type, value.airconditioner.equipment_state)
+yy.time_slot_change()
 
-
-floor.floor_time_slot_shift()
-for key, value in main_corridor.items():
-    print(value.light.equipment_type, value.light.equipment_state)
-    print(value.airconditioner.equipment_type, value.airconditioner.equipment_state)
-
-print(floor.get_floor_equipment_units())
-
-# main_corridor = floor.floor_corridor_map["main_corridors"]
-# for key, value in main_corridor.items():
-
-#     print(value.airconditioner.equipment_type, value.airconditioner.power_consumption, value.airconditioner.equipment_state)
-
-#     value.airconditioner.change_equipment_state() 
-
-
-
-#     print(value.airconditioner.equipment_type, value.airconditioner.power_consumption, value.airconditioner.equipment_state)
+print(yy.floor_map[1].get_floor_equipment_units())
+ 
