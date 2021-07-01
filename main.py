7fb23f46c9fee4aefe139628b2c65c2ebf59f059
anantharaman445 <anantharaman45@gmail.com>
2021-07-01 21:07:49 +0530

    main changed

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 
 
-# from hotel_automation import HotelAutomation as ha
+from hotel_automation import HotelAutomation as ha
 
-# yy = ha(2,1,2)
-# yy.time_slot_change()
-# yy.print_floor_states()
-# yy.movement_change(1, 2)
-# yy.print_floor_states()
-# yy.movement_change(1, 2, "NO_MOVEMENT")
-# yy.print_floor_states()
+yy = ha(2,1,2)
+yy.time_slot_change()
+yy.print_floor_states()
+yy.movement_change(1, 2)
+yy.print_floor_states()
+yy.movement_change(1, 2, "NO_MOVEMENT")
+yy.print_floor_states()
 
 #this is a dummy file to be removed
