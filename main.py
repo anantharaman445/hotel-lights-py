8f38f6cc95d7c043b8a0103cbab83016baf36bdf
anantharaman445 <anantharaman45@gmail.com>
2021-07-01 20:20:16 +0530

    test cases added

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 
 
-from hotel_automation import HotelAutomation as ha
+# from hotel_automation import HotelAutomation as ha
 
-yy = ha(1,1,2)
+# yy = ha(1,1,2)
+# # yy.print_floor_states()
+# yy.time_slot_change()
 # yy.print_floor_states()
-yy.time_slot_change()
-yy.print_floor_states()
-yy.movement_change(1, 2)
-yy.print_floor_states()
-yy.movement_change(1, 2, "NO_MOVEMENT")
-yy.print_floor_states()
+# yy.movement_change(1, 2)
+# yy.print_floor_states()
+# yy.movement_change(1, 2, "NO_MOVEMENT")
+# yy.print_floor_states()
+
+#this is a dummy file to be removed
