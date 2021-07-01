912e9635c0af46a268154c7e791f14218fa40f7a
anantharaman445 <anantharaman45@gmail.com>
2021-07-01 15:28:01 +0530

    time slot changes added

--- hotel_automation.py
+++ hotel_automation.py
@@ -1,11 +1,17 @@
 from Utils.floor_utils import add_floors
-
+from Config import hotel_management_constants
 
 class HotelAutomation:
     def __init__(self, no_of_floors, main_corridors_per_floor, sub_corridor_per_floor):
         self.floor_map = add_floors(no_of_floors, main_corridors_per_floor, sub_corridor_per_floor)
+        self.time_slot = hotel_management_constants["TIMESLOT"]["DAY"]
 
     def time_slot_change(self):
+        if self.time_slot == hotel_management_constants["TIMESLOT"]["DAY"]:
+             self.time_slot = hotel_management_constants["TIMESLOT"]["NIGHT"]
+        else:
+            self.time_slot = hotel_management_constants["TIMESLOT"]["DAY"]
+        
         for floor_id, floor in self.floor_map.items():
             floor.floor_time_slot_shift()
     
--- main.py
+++ main.py
@@ -8,7 +8,12 @@ yy = ha(1,1,2)
 
 print(yy.floor_map[1].get_floor_equipment_units(), yy.floor_map[1].maximum_allowed_units)
 
+print(yy.time_slot)
 yy.time_slot_change()
-
+print(yy.time_slot)
 print(yy.floor_map[1].get_floor_equipment_units(), yy.floor_map[1].maximum_allowed_units)
  
+print(yy.time_slot)
+yy.time_slot_change()
+print(yy.time_slot)
+print(yy.floor_map[1].get_floor_equipment_units(), yy.floor_map[1].maximum_allowed_units)
