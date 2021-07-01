9256f4d9833f5ecf7fa49802819605190b164993
anantharaman445 <anantharaman45@gmail.com>
2021-07-01 15:52:55 +0530

    ac conditioners added

--- Assets/subcorridor.py
+++ Assets/subcorridor.py
@@ -4,5 +4,8 @@ from Assets.corridor import Corridor
 class SubCorridor(Corridor):
    
     def change_sub_corridor_light_state(self):
-        self.light.change_equipment_state() 
+        self.light.change_equipment_state()
+    
+    def change_sub_corridor_airconditioner_state(Self):
+        self.airconditioner.change_equipment_state()
 
